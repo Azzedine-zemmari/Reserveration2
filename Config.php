d1895e4f72adee1e8c06e2c60d4ecb97ed0c520a
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-12-30 12:57:56 +0100

    ..

--- Config.php
+++ Config.php
@@ -7,29 +7,23 @@ class Connection {
     private $userName = "root";
     private $userPass = "Azzedine2004";
 
-    private $db = null;
+    private $db;
 
     public function getConnection() {
         try {
-            // Check if the connection is already established
-            if ($this->db === null) {
                 $this->db = new PDO(
                     "mysql:host=".$this->host.";dbname=".$this->dbName, 
                     $this->userName, 
                     $this->userPass
                 );
                 $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-            return $this->db; // Return the database connection
+            
+                return $this->db; // Return the database connection
 
         } catch (PDOException $e) {
             die("Connection failed: " . $e->getMessage());
         }
     }
 
-    // Optional: A method to close the connection (good practice)
-    public function closeConnection() {
-        $this->db = null;
-    }
 }
 ?>
